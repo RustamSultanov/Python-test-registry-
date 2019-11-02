145b701fdc4b7aa9167c6e064b900b316e9823cb
Exvir <enotukit@gmail.com>
2019-11-02 19:14:35 +0300

    Новый неработающий фронт, собственно

--- registry/registry/settings.py
+++ registry/registry/settings.py
@@ -13,13 +13,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import environ
 
-env = environ.Env(DEBUG=(bool, False),)
-# reading .env file
-environ.Env.read_env()
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env = environ.Env(DEBUG=(bool, False),)
+# reading .env file
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -46,6 +45,7 @@ INSTALLED_APPS = [
     'django_registration',
     'mptt',
     'crispy_forms',
+#    'widget_tweaks',
     #custom apps
     'registry',
     'smart_contract',
